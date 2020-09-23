6868d96a30faa59eef073d15f5bac0b7070eee3c
Tom Mason <tom.mason14@gmail.com>
2020-09-23 13:44:54 +1000

    Update split_clusters.py

--- split_clusters.py
+++ split_clusters.py
@@ -23,6 +23,8 @@ with open(sys.argv[1]) as f:
             clusters.append(tmp)
             tmp = []
         tmp.append(line)
+    # last step
+    clusters.append(tmp)
 
 if not os.path.isdir("clusters"):
     os.mkdir("clusters")
